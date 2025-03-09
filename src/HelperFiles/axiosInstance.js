74352573fa52a634c8b97873fd9f0c8fc542cbe8
daminim2004 <brijendratiwari993@gmail.com>
2025-03-09 23:16:25 +0530

    port changed 3

--- src/HelperFiles/axiosInstance.js
+++ src/HelperFiles/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://bakend-production.up.railway.app",
+  baseURL: "https://bakend.up.railway.app",
   withCredentials: true, // Important for sending cookies
 });
 
