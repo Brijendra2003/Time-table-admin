65304834f60140dad4e3a0c20b2d5f69e3a5e3c3
daminim2004 <brijendratiwari993@gmail.com>
2025-03-09 13:40:18 +0530

    initial co

--- src/HelperFiles/axiosInstance.js
+++ src/HelperFiles/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://timetable-production-a1c9.up.railway.app:5000",
+  baseURL: "https://timetable-production-a1c9.up.railway.app:5000",
   withCredentials: true, // Important for sending cookies
 });
 
