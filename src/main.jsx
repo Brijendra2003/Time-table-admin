290d5e59fbf5a14f6f35d4d3730d52699d43c24b
daminim2004 <mishrdamini@gmail.com>
2025-03-10 19:44:49 +0530

    Browser router basename

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-<HashRouter>
-  <App />
-</HashRouter>
+<BrowserRouter basename="/Time-table-admin">
+<App/>
+</BrowserRouter>
 );
