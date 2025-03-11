1748fa8f533fe4c466d3248eca516c4e8fbaf5f6
daminim2004 <brijendratiwari993@gmail.com>
2025-03-11 12:55:33 +0530

    Title updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import './index.css'
+import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-<BrowserRouter basename="/Time-table-admin">
-<App/>
-</BrowserRouter>
+  <BrowserRouter basename="/Time-table-admin">
+    <App />
+  </BrowserRouter>
 );
